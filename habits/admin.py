d2f3926496342ff0f1ba1dd19ab9b98017e5d8eb
Ahmad <ahmad.waliullah.khan@gmail.com>
2020-02-08 23:16:28 +0530

    fix for admin

--- habits/admin.py
+++ habits/admin.py
@@ -4,9 +4,15 @@ from .models import Habit, Tracker
 
 @admin.register(Tracker)
 class HabitsAdmin(admin.ModelAdmin):
-	fields = ('title', 'habit', 'description', 'user', 'done')
+	fields = ('habit', 'description', 'user', 'done')
 
-	list_display = ('title', 'habit', 'description', 'user', 'created_at', 'updated_at')
+	list_display = ('habit', 'description', 'user', 'started_at', 'updated_at' , 'is_complete')
+
+	def is_complete(self, instance):
+		if instance.done == True:
+			return "Completed"
+		else:
+			return "On Going"
 
 	def get_user(self):
 		pass
@@ -15,7 +21,7 @@ class HabitsAdmin(admin.ModelAdmin):
 class HabitsAdmin(admin.ModelAdmin):
 	fields = ('title', 'description', 'user')
 
-	list_display = ('title', 'description', 'user', 'created_at', 'updated_at')
+	list_display = ('title', 'description', 'user', 'started_at', 'updated_at')
 
 	def get_user(self):
 		pass
--- habits/models.py
+++ habits/models.py
@@ -7,8 +7,8 @@ Database representing the Habit Tracker
 class Habit(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	title = models.CharField(max_length=200)
-	description = models.CharField(max_length=200)
-	created_at = models.DateTimeField(auto_now_add=True)
+	description = models.TextField()
+	started_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 
 	def __str__(self):
@@ -16,20 +16,19 @@ class Habit(models.Model):
 
 	class Meta:
 		verbose_name = "habit"
-		verbose_name_plural = "Habits"
+		verbose_name_plural = "Habit Management"
 
 class Tracker(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	habit = models.ForeignKey(Habit, on_delete=models.CASCADE)
-	description = models.CharField(max_length=200)
-	title = models.CharField(max_length=200)
-	created_at = models.DateTimeField(auto_now_add=True)
+	description = models.TextField()
+	started_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 	done = models.BooleanField(default=False)
 
 	def __str__(self):
-		return "{}".format(self.title)
+		return "{}".format(self.habit)
 
 	class Meta:
 		verbose_name = "tracker"
-		verbose_name_plural = "Trackers"
+		verbose_name_plural = "Track Habits"
